7398f0011ec12af1056b4a0f18282f04d3885f83
samdang03 <quandh03@gmail.com>
2026-02-15 01:41:25 -0800

    Added electrolye vs conductvity scatter matrix

--- Wetlands_Metadata_Preliminary_Analysis.R
+++ Wetlands_Metadata_Preliminary_Analysis.R
@@ -8,7 +8,6 @@ library(psych)
 
 metafp <- "wetlands_metadata.txt"
 meta_wetlands <- read_delim(metafp, delim = "\t")
-http://127.0.0.1:23559/graphics/plot_zoom_png?width=1904&height=970
 meta_scatter_data <- meta_wetlands %>%
   select(conductivity,
          exchange_capacity,
@@ -22,7 +21,9 @@ meta_scatter_data <- meta_wetlands %>%
          potential_net_nitrification) %>%
   mutate(across(everything(),
                 ~ suppressWarnings(as.numeric(.))))%>%
-  filter(ch4_flux < 0.2)
+  filter(potential_net_n_mineralization < 10 |
+           is.na(potential_net_n_mineralization))
+  
 
 Scatter_Matrix <- ggpairs(
   meta_scatter_data,
@@ -33,3 +34,27 @@ Scatter_Matrix
 
 ggsave("scatter plot matrix.png", Scatter_Matrix, 
        width = 7, height = 7)
+
+cond_vs_soilnh4 <- meta_scatter_data %>%
+  ggplot(aes(x = conductivity, y = soil_nh4)) +
+  geom_point() +
+  labs(x = "Conductivity (mS/cm)",
+    y = "Soil NH4 (µg N per g dry soil)")
+cond_vs_soilnh4
+
+ggsave("Conductivity vs Soil NH4.png", cond_vs_soilnh4, 
+       width = 7, height = 7)
+
+cond_vs_n_mineralization <- meta_scatter_data %>%
+  ggplot(aes(x = conductivity, y = potential_net_n_mineralization)) +
+  geom_point() +
+  geom_vline(xintercept = 0.3,
+             linetype = "dashed",
+             linewidth = 1) +
+  labs(x = "Conductivity (mS/cm)",
+       y = "N Mineralization (ug N per g dry soil per day)")
+cond_vs_n_mineralization
+
+ggsave("Conductivity vs Nitrogen Mineralization.png", cond_vs_n_mineralization, 
+       width = 7, height = 7)
+
