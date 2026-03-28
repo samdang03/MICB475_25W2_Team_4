8dc88391e08a67d1bb8fb88c225bf0d6a1bfc3e2
alorrain <alorrain@student.ubc.ca>
2026-03-27 21:43:17 -0700

    Extended code, added Wilcoxon Test

--- MICB 475 - Family Taxonomic Analysis.R
+++ MICB 475 - Family Taxonomic Analysis.R
@@ -3,6 +3,8 @@ library(ape)
 library(tidyverse)
 library(ggplot2)
 library(picante)
+library(ggpubr)
+library(dplyr)
 
 #Initial loading (code adapted from Ruth + Richard)
 metafp <- "Phyloseq_Files/wetlands_metadata_NEW 2.txt" 
@@ -61,31 +63,83 @@ unique_family
 phylo_desulfo_f <- subset_taxa(phylo_nmnc, Family == "f__Thermodesulfobacteriaceae")
 get_taxa_unique(phylo_desulfo_f, "Genus")
 #There's only one genus (Thermosulfuriphilus), so this will not be continued with
-#The final plot will be focused entirely on families within the Thermodesulfobacteriota phylum
+#The final plots will be focused entirely on families within the Thermodesulfobacteriota phylum
 
 #Filtering taxonomy table to top 10 + other
-phylo_desulfo_family <- tax_glom(phylo_desulfo, taxrank = "Family")
+phylo_desulfo <- subset_taxa(phylo_nmnc, Phylum =="p__Thermodesulfobacteriota")
 
-top_10 <- names(sort(taxa_sums(phylo_desulfo_family), TRUE)[1:10])
+ps_fam <- tax_glom(phylo_desulfo, taxrank = "Family")
+
+top10 <- names(sort(taxa_sums(ps_fam), TRUE)[1:10])
+
+top10_names <- as.character(tax_table(ps_fam)[top10, "Family"])
+top10_names
 
 tax_table(phylo_desulfo_family)[!taxa_names(phylo_desulfo_family) %in% top_10, "Family"] <- "Other"
 
-#Rarefaction
-phylo_desulfo_rarefied <- rarefy_even_depth(phylo_desulfo_family, sample.size = 1000, rngseed = 42, replace = FALSE) 
+#Relative abundance
+phylo_desulfo_relative <- transform_sample_counts(phylo_desulfo_family, function(x) x / sum(x))
 
 #Plot creation
-phylo_plot <- plot_bar(phylo_desulfo_rarefied, fill = "Family") +
+phylo_plot_1 <- plot_bar(phylo_desulfo_relative, fill = "Family") +
   facet_wrap(~conductivity_category, scales = "free_x") +
   theme_classic() +
   geom_bar(aes(fill = Family), stat = "identity", position = "stack", color = NA) +
-  labs(title = "Relative Abundance of Microbial Families in Thermodesulfobacteriaceae Phylum",
+  labs(title = "Relative Abundance of Families in Thermodesulfobacteriaceae Phylum",
+       subtitle = "Grouped by Sediment Conductivity Category",
+       x = "Individual Samples",
+       y = "Relative Abundance (%)") +
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank()) 
+phylo_plot_1
+
+ggsave("results/taxonomy_family_plot.png", plot = phylo_plot_1, width = 10, height = 5, dpi = 300)
+
+
+#Filtering taxonomy table to desulfosbulbaceae + other
+phylo_desulfo_family <- tax_glom(phylo_desulfo, taxrank = "Family")
+
+desulfo_only <- names(sort(taxa_sums(phylo_desulfo_family), TRUE)[9])
+desulfo_only
+
+tax_table(phylo_desulfo_family)[!taxa_names(phylo_desulfo_family) %in% desulfo_only, "Family"] <- "Other"
+
+#Relative abundance
+phylo_desulfo_relative <- transform_sample_counts(phylo_desulfo_family, function(x) x / sum(x))
+
+#Plot creation
+phylo_plot_2 <- plot_bar(phylo_desulfo_relative, fill = "Family") +
+  facet_wrap(~conductivity_category, scales = "free_x") +
+  theme_classic() +
+  geom_bar(aes(fill = Family), stat = "identity", position = "stack", color = NA) +
+  labs(title = "Relative Abundance of Desulfobulbaceae Family in Thermodesulfobacteriaceae Phylum",
     subtitle = "Grouped by Sediment Conductivity Category",
     x = "Individual Samples",
     y = "Relative Abundance (%)") +
   theme(axis.text.x = element_blank(),
          axis.ticks.x = element_blank()) 
 
-phylo_plot
+phylo_plot_2
 
-ggsave("results/taxonomy_family_barplot.png", plot = phylo_plot, width = 10, height = 5, dpi = 300)
+ggsave("results/taxonomy_desulfobulbaceae_relative_plot.png", plot = phylo_plot_2, width = 10, height = 5, dpi = 300)
+
+#Wilcoxon box plot
+desulfo_df <- phylo_desulfo_relative %>%
+  psmelt()
+
+desulfo_wilcox_plot <- ggplot(desulfo_df, aes(x=conductivity_category, y=Abundance, fill=conductivity_category)) + 
+  geom_boxplot(outlier.shape = NA, alpha = 0.7) + 
+  geom_jitter(width=0.2, alpha = 0.5) + 
+  theme_classic() + 
+  scale_fill_manual(values = c("high" = "red", "low" = "blue")) + 
+  labs(
+    title = "Abundance of Desulfobulbaceae Family by Conductivity Category",
+    x = "Conductivity Category",
+    y = "Relative Abundance", 
+    subtitle = "Non-parametric Wilcoxon Rank-Sum Test") + 
+  stat_compare_means(method = "wilcox.test", label = "p.signif", label.x = 1.5)
+
+desulfo_wilcox_plot
+
+ggsave("results/desulfobulbaceae_wilcox_plot.png", plot = desulfo_wilcox_plot, width = 7, height = 5, dpi = 300)
 
